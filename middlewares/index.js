ad882f1670ff9e9ab84f669198391830ba4da2e7
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-04 16:35:48 +0200

    refactor: rename middlewares (remove .middleware)

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,13 +1,11 @@
-const controllerWrapper = require("./controllerWrapper.middleware");
-const authenticate = require("./authenticate.middleware");
-const validateBody = require("./validateBody.middleware");
-const isValidId = require("./isValidId.middleware");
-const upload = require("./upload.middleware");
+const authenticate = require("./authenticate");
+const validateBody = require("./validateBody");
+const isValidId = require("./isValidId");
+const upload = require("./uploadImage");
 const urlNotFound = require("./urlNotFound");
 const errorHandler = require("./errorHandler");
 
 module.exports = {
-  controllerWrapper,
   authenticate,
   validateBody,
   isValidId,
--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,9 +1,10 @@
+const { HttpError } = require("../helpers");
+
 const validateBody = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      error.status = 400;
-      throw error;
+      next(HttpError(400, error.message));
     }
 
     next();
